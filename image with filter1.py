21b5032f3bde302152411e431ccd8113ae4c2c3c
Ritsu Ueda <ritsuueda@JUs-MBA20.local>
2022-07-06 17:24:34 +0900

    Fixed filter

--- image with filter1.py
+++ image with filter1.py
@@ -3,36 +3,43 @@ import matplotlib.pyplot as plt
 from RSAinternship import *
 import cv2
 
-gazo = cv2.imread( "/Users/ritsuueda/Desktop/images/kadai4.bmp", 0 )
-sample_gazo = cv2.imread( "/Users/ritsuueda/Desktop/images/kadai4.bmp", 0 )
+gazo = cv2.imread( "/Users/ritsuueda/Desktop/images/7134.png", 0 )
+sample_gazo = cv2.imread("/Users/ritsuueda/Desktop/images/7134.png", 0 )
 pbk, pvk, n = key_gen(6)
 
+# Resize image
+gazo = cv2.resize(gazo, dsize = None, fx = 0.05, fy = 0.05)
+sample_gazo = cv2.resize(sample_gazo, dsize = None, fx = 0.05, fy = 0.05)
+height, width = gazo.shape
+print(height)
+print(width)
+
 # Print original image
-print("Original image")
+print( "Original image" )
 print( gazo )
 plt.figure()
-plt.imshow(gazo, cmap="gray", vmin=0, vmax=255, interpolation="None")
+plt.imshow(gazo, cmap="gray", vmin=0, vmax=255)
 gazo = gazo.tolist()
 
 # Encrypt image
-for y in range(12):
-  for x in range(12):
+for y in range(height):
+  for x in range(width):
       gazo[y][x] = Enc(pbk, n, gazo[y][x])
 
 gazo = np.array(gazo)
 
 # Print encrypted image
-print("Encrypted image")
+print( "Encrypted image" )
 print(gazo)
 plt.figure()
-plt.imshow(gazo, cmap="gray", vmin=0, vmax=255, interpolation="None")
+plt.imshow(gazo, cmap="gray", vmin=0, vmax=255)
 
 # Apply filter to both images
-gazo2 = np.zeros((12,12))
-sample_filter_gazo = np.zeros((12,12))
+gazo2 = np.zeros((height,width))
+sample_filter_gazo = np.zeros((height, width))
 gazo2 = gazo2.tolist()
-for x in range(1,11):
-    for y in range(1,11):
+for x in range(1,(width-1)):
+    for y in range(1,(height-1)):
         filter = [
             [1/9, 1/9, 1/9],
             [1/9, 1/9, 1/9],
@@ -42,43 +49,42 @@ for x in range(1,11):
         gasochi_sample = 0
         for xx in range(3):
             for yy in range(3):
-              # Apply filter to sample image that is not encrypted
-              gasochi_sample += sample_gazo[y+yy-1][x+xx-1] * filter[yy][xx]
+                # Apply filter to sample image that is not encrypted
+                gasochi_sample += sample_gazo[y+yy-1][x+xx-1] * filter[yy][xx]
 
-              # Apply filter to encrypted image
-              filter[yy][xx] = Enc(pbk, n, filter[yy][xx]*9)
-              gasochi.append(gazo[y+yy-1][x+xx-1] * filter[yy][xx])
+                # Apply filter to encrypted image
+                filter[yy][xx] = Enc(pbk, n, filter[yy][xx]*18)              
+                gasochi.append(gazo[y+yy-1][x+xx-1] * filter[yy][xx])
         gazo2[y][x] = gasochi
         sample_filter_gazo[y][x] = int(gasochi_sample)
 
 # Print list of encrypted image that has the filter applied
-print("Encrypted image w/ filter")
+print( "Encrypted image w/ filter" )
 print( gazo2 )
 
-# Decrypt image
-for y in range(12):
-  for x in range(12):
-    if 11 > x >= 1 and 11 > y >= 1:
+# Decrypt image 
+for y in range(height):
+  for x in range(width):
+    if (width-1) > x >= 1 and (height-1) > y >= 1:
       gasochi3 = 0
       for z in range(9):
         g = gazo2[y][x][z]
-        gasochi2 = Dec(pvk, n, int(g))/9
+        gasochi2 = Dec(pvk, n, int(g))/18
         gasochi3 += gasochi2
       gazo2[y][x] =  int(gasochi3)
-    else:
+    else: 
       gazo2[y][x] = int(Dec(pvk, n, gazo2[y][x]))
 
 # Print decrypted image
 gazo2 = np.array(gazo2)
-print("Decrypted image")
+print( "Decrypted image" )
 print( gazo2 )
 plt.figure()
-plt.imshow(gazo2, cmap="gray", vmin=0, vmax=255, interpolation="None")
-
+plt.imshow(gazo2, cmap="gray", vmin=0, vmax=255)
 
 # Print sample image for comparison
-print("sample image(no encryption)")
+print("Sample image(no encryption)")
 print( sample_filter_gazo )
 plt.figure()
-plt.imshow(sample_filter_gazo, cmap="gray", vmin=0, vmax=255, interpolation="None")
+plt.imshow(sample_filter_gazo, cmap="gray", vmin=0, vmax=255)
 plt.show()
--- image with filter2.py
+++ image with filter2.py
@@ -1,45 +1,57 @@
+from random import sample
 import numpy as np
 import matplotlib.pyplot as plt
 from RSAinternship import *
 import cv2
 
-gazo = cv2.imread( "/Users/ritsuueda/Desktop/images/7134.png", 0 )
-sample_gazo = cv2.imread("/Users/ritsuueda/Desktop/images/7134.png", 0 )
-pbk, pvk, n = key_gen(6)
+gazo = cv2.imread( "/Users/ritsuueda/Desktop/images/2098_color.png", 3 )
+gazo = cv2.cvtColor(gazo, cv2.COLOR_BGR2RGB)
+sample_gazo = cv2.imread( "/Users/ritsuueda/Desktop/images/2098_color.png", 3 )
+sample_gazo = cv2.cvtColor(sample_gazo, cv2.COLOR_BGR2RGB)
+pbk, pvk, n = key_gen(8)
 
 # Resize image
-gazo = cv2.resize(gazo, dsize = None, fx = 0.05, fy = 0.05)
-sample_gazo = cv2.resize(sample_gazo, dsize = None, fx = 0.05, fy = 0.05)
-height, width = gazo.shape
-print(height)
-print(width)
+gazo = cv2.resize(gazo, dsize = None, fx = 0.5, fy = 0.5)
+sample_gazo = cv2.resize(sample_gazo, dsize = None, fx = 0.5, fy = 0.5)
+height, width , c = gazo.shape
+
+sample_filter_gazo = []
+for i in range(height):
+    sample_filter_gazo.append([])
+    for j in range(width):
+        sample_filter_gazo[i].append([])
+        for k in range(c):
+            sample_filter_gazo[i][j].append(0)
 
 # Print original image
 print( "Original image" )
 print( gazo )
 plt.figure()
-plt.imshow(gazo, cmap="gray", vmin=0, vmax=255)
+plt.imshow(gazo)
 gazo = gazo.tolist()
 
-# Encrypt image
+# Encrypt image 
 for y in range(height):
   for x in range(width):
-      gazo[y][x] = Enc(pbk, n, gazo[y][x])
-
-gazo = np.array(gazo)
+    for z in range(c):
+      gazo[y][x][z] = Enc(pbk, n, gazo[y][x][z])
 
 # Print encrypted image
+gazo = np.array(gazo)
 print( "Encrypted image" )
 print(gazo)
 plt.figure()
-plt.imshow(gazo, cmap="gray", vmin=0, vmax=255)
+plt.imshow(gazo)
 
-# Apply filter to both images
-gazo2 = np.zeros((height,width))
-sample_filter_gazo = np.zeros((height, width))
+
+
+gazo2 = np.zeros((height,width, c))
 gazo2 = gazo2.tolist()
+
+# Apply filter to both images 
 for x in range(1,(width-1)):
     for y in range(1,(height-1)):
+      for z in range(3):
         filter = [
             [1/9, 1/9, 1/9],
             [1/9, 1/9, 1/9],
@@ -49,42 +61,52 @@ for x in range(1,(width-1)):
         gasochi_sample = 0
         for xx in range(3):
             for yy in range(3):
-                # Apply filter to sample image that is not encrypted
-                gasochi_sample += sample_gazo[y+yy-1][x+xx-1] * filter[yy][xx]
+                # Apply filter to sample image
+                gasochi_sample += sample_gazo[y+yy-1][x+xx-1][z] * filter[yy][xx]
 
-                # Apply filter to encrypted image
-                filter[yy][xx] = Enc(pbk, n, filter[yy][xx]*18)              
-                gasochi.append(gazo[y+yy-1][x+xx-1] * filter[yy][xx])
-        gazo2[y][x] = gasochi
-        sample_filter_gazo[y][x] = int(gasochi_sample)
-
-# Print list of encrypted image that has the filter applied
-print( "Encrypted image w/ filter" )
-print( gazo2 )
+                # Apply filter to encrypted image 
+                filter[yy][xx] = Enc(pbk, n, filter[yy][xx]*27)              
+                gasochi.append(gazo[y+yy-1][x+xx-1][z] * filter[yy][xx])
+        gazo2[y][x][z] = gasochi
+        sample_filter_gazo[y][x][z] = int(gasochi_sample)
 
 # Decrypt image 
 for y in range(height):
   for x in range(width):
-    if (width-1) > x >= 1 and (height-1) > y >= 1:
-      gasochi3 = 0
-      for z in range(9):
-        g = gazo2[y][x][z]
-        gasochi2 = Dec(pvk, n, int(g))/18
-        gasochi3 += gasochi2
-      gazo2[y][x] =  int(gasochi3)
-    else: 
-      gazo2[y][x] = int(Dec(pvk, n, gazo2[y][x]))
+    for z in range(c):
+      if (width-1) > x >= 1 and (height-1) > y >= 1:
+        gasochi3 = 0
+        for zz in range(9):
+          g = gazo2[y][x][z][zz]
+          gasochi2 = Dec(pvk, n, int(g))/27
+          gasochi3 += gasochi2
+        gazo2[y][x][z] =  int(gasochi3)
+      else: 
+        gazo2[y][x][z] = int(Dec(pvk, n, int(gazo2[y][x][z])))
 
 # Print decrypted image
 gazo2 = np.array(gazo2)
 print( "Decrypted image" )
 print( gazo2 )
 plt.figure()
-plt.imshow(gazo2, cmap="gray", vmin=0, vmax=255)
+plt.imshow(gazo2)
 
 # Print sample image for comparison
+sample_filter_gazo = np.array(sample_filter_gazo)
 print("Sample image(no encryption)")
 print( sample_filter_gazo )
 plt.figure()
-plt.imshow(sample_filter_gazo, cmap="gray", vmin=0, vmax=255)
+plt.imshow(sample_filter_gazo)
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
